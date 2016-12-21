7864c176b31cade3f94cacae5a8d822914744565
yanghaa <yanghaa@sina.com>
2016-12-22 01:38:58 +0800

    add popover

--- emc/policy/browser/static/emc-legacy-custom.js
+++ emc/policy/browser/static/emc-legacy-custom.js
@@ -1,14 +1,28 @@
 require([
-  'jquery'
-], function($) {
+  'jquery','bootstrap-tabs'
+], function($,tabs) {
   'use strict';
-$(document).ready(function(){ 
+$(document).ready(function(){
 $('.collapse-control').on('click', function () {
 var willshow = $(this).find('.hidden');
 var willhide = $(this).find('.shown');
  willshow.removeClass('hidden').addClass('shown');
  willhide.removeClass('shown').addClass('hidden');
 });
-$('a[href^="http://mw4024.wicp.net:8089/"]').attr('target','_blank').attr('class','outputlink');
+$('a[href^="http://kwsensen.f3322.net:8089/"]').attr('target','_blank').attr('class','outputlink');
+// tab part
+$(".nav-tabs a").mouseover(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  });
+$(".nav-tabs").on("click","a",function (e) {
+    e.preventDefault();
+    var url = $(this).attr("data-js-target");
+    window.location.href = url;
+    return false;
+  });
+// popover
+%("[data-toggle='popover']").popover();
+
 });
 });
