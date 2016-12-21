bb7b286b59c7b0f7a22763966c8db8c28276ed20
yanghaa <yanghaa@sina.com>
2016-12-22 02:34:59 +0800

    popover

--- emc/policy/browser/static/emc-legacy-custom.js
+++ emc/policy/browser/static/emc-legacy-custom.js
@@ -22,7 +22,11 @@ $(".nav-tabs").on("click","a",function (e) {
     return false;
   });
 // popover
-%("[data-toggle='popover']").popover();
+$("[data-toggle='popover']").popover({
+  trigger:"hover",
+  placement:"right"
+  html="true"
+});
 
 });
 });
